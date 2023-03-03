ada050c75624b2db1f237a0e75fed12d66edff1e
alaminsahed <alaminsahed97@gmail.com>
2023-03-03 17:16:54 +0600

    chore: input test

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -33,6 +33,7 @@ const InputBox = () => {
         <button onClick={() => taskSubmitHandler()} className='btn btn-info'>
           Add
         </button>
+        <p>it's working or not</p>
       </div>
     </div>
   );
