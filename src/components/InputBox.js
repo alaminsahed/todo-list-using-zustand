c084825f943ed1c812aece9bd8dace3303fb72b8
alaminsahed <alaminsahed97@gmail.com>
2023-03-03 15:12:10 +0600

    chore: fix preitter

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,33 +1,41 @@
 import React, { useState } from 'react';
 import useStore from '../store';
 
-
 const InputBox = () => {
-    const [input, setInput] = useState('');
+  const [input, setInput] = useState('');
 
-    const addTask = useStore(state => state.addTask);
+  const addTask = useStore((state) => state.addTask);
 
-    const taskSubmitHandler = () => {
-        console.log(input);
-        addTask({
-            id: Date.now(),
-            task: input,
-        })
-        setInput('');
-    }
+  const taskSubmitHandler = () => {
+    console.log(input);
+    addTask({
+      id: Date.now(),
+      task: input,
+    });
+    setInput('');
+  };
 
-    return (
-        <div className=' d-flex justify-content-center mt-5'>
-            <div>
-                <input type="text" value={input} onChange={(e) => { setInput(e.target.value) }} placeholder="Add Your Task" className='border border-2 rounded rounded-1 border-dark p-1' />
-            </div>
+  return (
+    <div className=' d-flex justify-content-center mt-5'>
+      <div>
+        <input
+          type='text'
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          placeholder='Add Your Task'
+          className='border border-2 rounded rounded-1 border-dark p-1'
+        />
+      </div>
 
-            <div className='ms-3'>
-                <button onClick={() => taskSubmitHandler()} className='btn btn-info'>Add</button>
-            </div>
-
-        </div>
-    );
+      <div className='ms-3'>
+        <button onClick={() => taskSubmitHandler()} className='btn btn-info'>
+          Add
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default InputBox;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,25 +2,27 @@ import React from 'react';
 import useStore from '../store';
 
 const TaskList = () => {
-    const { tasks, removeTask } = useStore(state => ({
-        tasks: state.tasks,
-        removeTask: state.removeTask
-    }))
-    return (
-        <div>
-            {
-                tasks.map(task => (
-                    <div key={task.id}>
-                        <div className='card mt-5 text-center shadow p-3 mb-5 bg-body rounded' style={{ width: "50%", marginLeft: "3rem" }}>
-                            <h1>{task.task}</h1>
-                            <button onClick={() => removeTask(task.id)} className="btn btn-primary">Delete</button>
-                        </div>
-                    </div>
-
-                ))
-            }
+  const { tasks, removeTask } = useStore((state) => ({
+    tasks: state.tasks,
+    removeTask: state.removeTask,
+  }));
+  return (
+    <div>
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <div
+            className='card mt-5 text-center shadow p-3 mb-5 bg-body rounded'
+            style={{ width: '50%', marginLeft: '3rem' }}
+          >
+            <h1>{task.task}</h1>
+            <button onClick={() => removeTask(task.id)} className='btn btn-primary'>
+              Delete
+            </button>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default TaskList;
