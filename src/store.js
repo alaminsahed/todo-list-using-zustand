18e11d8006e5bcc33cf887a125a3b6c14d7b6e69
alaminsahed <alaminsahed97@gmail.com>
2023-03-03 14:50:41 +0600

    chore: update eslint rules

--- src/store.js
+++ src/store.js
@@ -1,16 +1,21 @@
+/* eslint-disable prettier/prettier */
 import create from 'zustand';
-import { devtools, persist } from 'zustand/middleware'
-
+import { devtools, persist } from 'zustand/middleware';
 
 const useStore = create(
-    devtools(persist((set => ({
-        tasks: [],
-        addTask: (task) => set(state => ({ tasks: [...state.tasks, task] })),
-        removeTask: (id) => set(state => ({ tasks: state.tasks.filter(task => task.id !== id) })),
-
-    })), {
-        name: 'tasks',
-    }))
-)
+    devtools(
+        persist(
+            (set) => ({
+                tasks: [],
+                addTask: (task) => set((state) => ({ tasks: [...state.tasks, task] })),
+                removeTask: (id) =>
+                    set((state) => ({ tasks: state.tasks.filter((task) => task.id !== id) }))
+            }),
+            {
+                name: 'tasks'
+            }
+        )
+    )
+);
 
 export default useStore;
